65502affac24de985370a50f60f54398f440d382
ofr1tz <ofr1tz@yahoo.de>
2018-08-17 10:46:25 +0200

    aesthetic changes

--- transit_evolution.R
+++ transit_evolution.R
@@ -1,18 +1,12 @@
-library(tidyverse)
-library(ggmap)
-library(osmdata)
-library(sf)
-library(magick)
+require(tidyverse)
+require(ggmap)
+require(osmdata)
+require(sf)
+require(magick)
 
-bbox <- c(29.95,
-          -2.10,
-          30.25,
-          -1.80)
+bbox <- c(29.95, -2.10, 30.25, -1.80)
 
-map <- get_map(location=bbox,
-               source="stamen",
-               color="bw", 
-               scale="auto")
+map <- get_map(location=bbox, source="stamen", color="bw", scale="auto")
 g <- ggmap(map, darken=c(.4, "white"))
 
 osmTimeSeries <- function(date) {
@@ -26,10 +20,9 @@ osmTimeSeries <- function(date) {
     
     g + geom_sf(data=routes, size=1, color="red", inherit.aes=FALSE) +
         labs(title=paste0("Kigali Bus Network on OpenStreetMap (", as.character(date), ")")) +
-        geom_text(x=bbox[3]-.002, y=bbox[2]+.002, 
-                  hjust=1, vjust=0, 
+        geom_text(x=bbox[3]-.002, y=bbox[2]+.002, hjust=1, vjust=0, 
                   label="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.",
-                  size=3.5, colour="grey60") +
+                  fontface="plain", size=3.5, colour="grey60") +
         theme(plot.title = element_text(face="bold", hjust=0.5, size=10, colour="grey25"),
               axis.title.y=element_blank(),
               axis.title.x=element_blank(),
@@ -38,17 +31,14 @@ osmTimeSeries <- function(date) {
               axis.ticks=element_line(color="grey60")) 
         
     ggsave(filename=paste0("./output/OSM_Kigali_Transit_", date,".png"),
-           device="png",
-           width = 10,
-           height=10,
-           dpi = 150)
+           device="png", width = 10, height=10, dpi = 150)
 }
 
 seq(as.Date("2018/02/23"), Sys.Date(), "week") %>% 
     map_df(osmTimeSeries)
 
-list.files(path = "./output/", pattern = "OSM_Kigali_Transit_*.png", full.names = T) %>% 
+list.files(path = "./output/", pattern = "*.png", full.names = T) %>% 
     map(image_read) %>% 
     image_join() %>% 
-    image_animate(fps=3) %>% 
+    image_animate(fps=4) %>% 
     image_write("./output/OSM_Kigali_Transit_evolution.gif")
